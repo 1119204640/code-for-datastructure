6cde597a31937673d92cdae4f440e039cbd4a8e1
LEON <61219917+1119204640@users.noreply.github.com>
2023-10-26 20:32:11 +0800

    Update BFS.cpp
    参考严蔚敏教材，用了更精准的描述

--- BFS.cpp
+++ BFS.cpp
@@ -1,75 +1,160 @@
 #include <iostream>
 using namespace std;
 
-#define n 5
+// #define INFINITY INT_MAX
+#define MAX_VERTEX_NUM 20
+#define MAXSIZE 20
 
-//循环队列，空：front == rear; 满：(rear + 1) % n == front
-typedef struct A
+typedef char VertexType;
+typedef int VRType;
+typedef VRType AdjMatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+typedef struct Graph
 {
-    int top = 0;
-    int rear = 0;
-    int data[n];
+    VertexType vexs[MAX_VERTEX_NUM];
+    AdjMatrix arc;
+    int vexnum, arcnum;
+}Graph;
+typedef struct Queue
+{
+    int *data;
+    int front, rear;
 }Queue;
 
+void InitQueue(Queue &Q) {
+    Q.data = (int*) malloc(sizeof(int) * MAXSIZE);
+    Q.front = 0;
+    Q.rear = 0;
+}
+
+bool IsFull(Queue Q) {
+    if ((Q.rear + 1) % MAXSIZE == Q.front) {
+        return true;
+    }
+    return false;
+}
+
+bool IsEmpty(Queue Q) {
+    if (Q.front == Q.rear) {
+        return true;
+    }
+    return false;
+}
+
+int EnQueue(Queue &Q, int v) {
+    if (!IsFull(Q)) { //若队列非满则入队
+        Q.data[Q.rear] = v;
+        Q.rear = (Q.rear + 1) % MAXSIZE;
+        return 1;
+    }
+    return  -1;
+}
+
+int DeQueue(Queue &Q) {
+    if (!IsEmpty(Q)) {  //若队列非空则出队
+        int e = Q.data[Q.front];
+        Q.front = (Q.front + 1) % MAXSIZE;
+        return e;
+    }
+    return -1;
+} 
+
+void Initial(Graph &G) {
+    cout << "enter the vertex number: ";
+    cin >> G.vexnum;
+    cout << "enter the arc number: ";
+    cin >> G.arcnum;
+    cout << endl;
+
+    //加入顶点
+    for (int i = 0; i < G.vexnum; i++) {
+        cout << "enter NO." << i+1 << " vertex's data(only one letter): ";
+        cin >> G.vexs[i];
+    }
+    cout << endl;
+
+    //初始化邻接矩阵
+    for (int i = 0; i < G.vexnum; i++) {
+        for (int j = 0; j < G.vexnum; j++) {
+            G.arc[i][j] = 0;
+        }
+    }
+
+    //加入边
+    for (int i = 0; i < G.arcnum; i++) {
+        int start, end;
+        cout << "enter the start, end index and the value of NO." << i+1 << " arc: ";
+        cin >> start >> end;
+        G.arc[start][end] = 1;
+    }
+    cout << endl;
+}
+
+void Print(Graph G) {
+    for (int i = 0; i < G.vexnum; i++) {
+        for (int j = 0; j < G.vexnum; j++) {
+            cout << G.arc[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+//返回下标为v的结点的指向的第一个结点的下标
+int FirstNeightbor(Graph G, int v) {
+    for (int w = 0; w < G.vexnum; w++) {
+        if (G.arc[v][w] != 0) {
+            return w;
+        }
+    }
+    return -1;
+}
+
+//返回下标为v的结点里面，下标为w的下一个结点下标
+int NextNeightbor(Graph G, int v, int w) {
+    for (int i = w+1; i < G.vexnum; ++i) {
+        if (G.arc[v][i] != 0)
+            return i;
+    }
+    return -1;
+}
+
+void BFS(Graph G, int v, bool visited[], Queue &Q) {
+    cout << G.vexs[v] << ",";
+    visited[v] = true;
+    EnQueue(Q, v);
+    while (!IsEmpty(Q)) {
+        int u = DeQueue(Q);
+        for (int w = FirstNeightbor(G,u); w >=0; w = NextNeightbor(G,u,w)) {
+            if (!visited[w]) {
+                cout << G.vexs[w] << ",";
+                visited[w] = true;
+                EnQueue(Q,w);
+            }
+
+        }
+    }
+}
+
+void BFSTraverse(Graph G) {
+    bool visited[G.vexnum];
+    for (int i = 0; i < G.vexnum; i++) {
+        visited[i] = false; //初始化标记数组
+    }
+    Queue Q;
+    InitQueue(Q);
+    for (int v = 0; v < G.vexnum; v++) {
+        if (!visited[v]) {
+            BFS(G,v,visited,Q);
+        }
+    }
+
+}
+
 int main() {
-    int nums[n][n] = {{0,0,1,0,1},
-                      {1,0,0,1,0},
-                      {0,0,0,0,1},
-                      {0,0,1,0,0},
-                      {0,0,0,0,0}};
-    int indeg[n] = {0};
-    int res[n];
-    int count = 0;
-    Queue q;
-
-    //create indegree
-    for (int j = 0; j < n; j++) {
-        for (int i = 0; i < n; i++) {
-            if (nums[i][j] == 1) {
-                indeg[j]++;
-            }
-        }
-    }
-
-    // check indegree
-    cout <<  "indegree: ";
-    for (int i = 0; i < n; i++) {
-        cout << indeg[i] << " ";
-    }
-    
-    while (count < 5){
-        for (int i = 0 ; i < n; i++) {
-            if (indeg[i] == 0) {  //选入度为0的入队
-                q.data[q.rear] = i;
-                q.rear = (q.rear + 1) % n;
-            }
-        }
-        while (q.top != q.rear) //队列非空
-        {
-            int curr = q.data[q.top];
-
-            for (int j = 0; j < n; j++) {
-                if (nums[curr][j] > 0) { //该点的出度不为0
-                    indeg[j]--;
-                }
-            }
-            res[count++] = curr;
-            indeg[curr] = -1; //把已出答案的节点下标在入度表里减1
-            q.top = (q.top + 1) % n;
-        }
-    }
-    
-
-    //check answer
-    cout << endl <<  "BFS: ";
-    for (int i = 0; i < n; i++) {
-        cout << res[i] << " ";
-    }
-    
-    //check indegree
-    cout << endl <<  "indegree: ";
-    for (int i = 0; i < n; i++) {
-        cout <<indeg[i] << " ";
-    }
+    Graph G;
+    Initial(G);
+    Print(G);
+    BFSTraverse(G);
     return 0;
 }
+
