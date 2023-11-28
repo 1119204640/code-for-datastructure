9c022715ee0c605df1e3b850470eb67539a4a874
LEON <61219917+1119204640@users.noreply.github.com>
2023-11-28 09:43:33 +0800

    Add files via upload

---
+++ Kruskal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+typedef struct Arc {
+    int start;
+    int end;
+    int value;
+}Arc, *Edge;
+
+void InitEdge(Edge e) {
+    cout << "enter the num of ars: ";
+}
+
+int main() {
+    
+    return 0;
+}
